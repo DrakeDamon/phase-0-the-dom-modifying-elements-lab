0456d9b5e5ea871fb6bcd1fd931c67b3fffa6d4b
Drake Damon <dddamon06@gmail.com>
2024-03-24 19:48:31 -0400

    completed the dom minipulation assigment by changing the dom is various ways

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+ // Remove the <main> element with the ID of "main"
+const mainElement = document.getElementById('main');
+mainElement.remove();
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Drake is the champion';
+document.body.appendChild(newHeader);
